8b36bf7076ed1cffa983a993488fbea0cfbb8889
SnowzTail <i@snowztail.com>
2018-11-20 22:45:13 +0000

    Update results

--- theory.m
+++ theory.m
@@ -14,36 +14,38 @@ manifold = spv(rxArrayPos, txDir);
 % normalised equal power sources -> unit diagonal; uncorrelated -> others
 % entries zero; size determined by number of transmitters
 covTx = eye(length(txDir));
-% additive uncorrelated isotropic noise, SNR = 20 dB
-varNoise = 1e-1;
+% additive uncorrelated isotropic noise, SNR = 40 dB
+varNoise = 1e-4;
 % covariance matrix of transmitted signal
 % diagonal -> signal power; others -> covariance of signals
 covRx = manifold * covTx * manifold' + varNoise * eye(length(rxArrayPos));
 %% Covariance matrix: practice
 load('xAudio'); load('xImage');
-audioReal = X_au;
-audioImag = X_im;
-% % % % % % soundsc(real(xAudioReal(2, :)), 11025);
-% % % % % % displayimage(xAudioImag(2, :),image_size, 201, 'The received signal at the 2nd antenna');
-covAudioReal = audioReal * audioReal' / length(audioReal(1, :));
-covAudioImag = audioImag * audioImag' / length(audioImag(1, :));
+signalAudio = X_au;
+signalImage = X_im;
+% % % soundsc(real(signalAudio(2, :)), 11025);
+% % % displayimage(signalImage(2, :),image_size, 201, 'The received signal at the 2nd antenna');
+covAudio = signalAudio * signalAudio' / length(signalAudio(1, :));
+covImage = signalImage * signalImage' / length(signalImage(1, :));
 % source information is actually unknown
 txDir = [];
 covTx = [];
 manifold = [];
 varNoise = [];
-%% Detection approach: eigendecomposition
+%% Detection: eigendecomposition
 % theoretical value
-eigTransmitted = eig(covRx);
+eigTheory = eig(covRx);
 % practical value
-eigAudioReal = eig(covAudioReal);
-eigAudioImag = eig(covAudioImag);
-%% Estimation
+eigAudio = eig(covAudio);
+eigImage = eig(covImage);
+%% Estimation: conventional approach
 % manifold vector of the desired source
-manifoldDesired = spv(rxArrayPos, [35, 0]);
+manifoldDesired = spv(rxArrayPos, [90, 0]);
 % optimum weight by Wiener-Hopf solution
 gain = 1;
 weightOptimum = gain * covRx \ manifoldDesired;
 patternOptimum = pattern(rxArrayPos, weightOptimum);
 plot2d3d(patternOptimum, 0: 180, 0, 'Gain in dB', 'Optimum  pattern by W-H Algorithm');
 legend('Original', 'Optimum');
+%% Estimation: superresolution approach
+% patternSuperres = music
